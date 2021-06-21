cbea9f75e31074c1dd2f0e8eb38fed3a97e9fa17
Lucas Dunhão <ldunhao@hotmail.com>
2021-06-21 15:50:24 -0300

    testando o commit do intelliJ

--- src/main/java/com/lucas/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/lucas/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -15,5 +15,6 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
         StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+
     }
 }
