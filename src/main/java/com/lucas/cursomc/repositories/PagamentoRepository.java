a5d2baa9f44e5ce3a0dac0126471da70e601128f
ldunhao <ldunhao@hotmail.com>
2021-07-08 13:39:13 -0300

    Pedido, Pagamento e Estado Pagamento

---
+++ src/main/java/com/lucas/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,10 @@
+package com.lucas.cursomc.repositories;
+
+import com.lucas.cursomc.domain.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+}
