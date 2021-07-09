d106c3cab37d6d34789e4ca4251ecf97d01ea00f
ldunhao <ldunhao@hotmail.com>
2021-07-09 14:55:16 -0300

    Categoria post endpoint

--- src/main/java/com/lucas/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/lucas/cursomc/resources/CategoriaResource.java
@@ -3,15 +3,14 @@ package com.lucas.cursomc.resources;
 import com.lucas.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.lucas.cursomc.domain.Categoria;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 
 @RestController
@@ -25,5 +24,12 @@ public class CategoriaResource {
     public ResponseEntity<?> find(@PathVariable Integer id){
         Categoria obj = service.find(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
